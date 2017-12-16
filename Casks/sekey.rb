855c3aba0d800c989db3f0425fdd56357dcad3fb
Nicolas Trippar <ntrippar@gmail.com>
2017-12-17 00:10:31 +0100

    added pkg, modified to automatically runs when the pkg is intalled.

--- Casks/sekey.rb
+++ Casks/sekey.rb
@@ -1,15 +1,15 @@
 cask 'sekey' do
   version '0.1'
-  sha256 '86cd69753de6a186f5991c7148014e8349ecc59d6bd15da19823a24e62faa4db'
+  sha256 '8d473f7214ba76b70ca30905e2ebd07d7c421f1cff320285bd8ec8d2a7a5b083'
 
-  url "https://github.com/ntrippar/sekey/releases/download/#{version}/SeKey-#{version}.zip"
+  url "https://github.com/ntrippar/sekey/releases/download/#{version}/SeKey-#{version}.pkg"
   appcast 'https://github.com/ntrippar/sekey/releases.atom',
           checkpoint: 'ed0d4a9db584f983da245bf83ec70ecdf1fe0e7109d10660b2dd43225e2f53f8'
   name 'SeKey'
   homepage 'https://github.com/ntrippar/sekey'
 
-  app 'SeKey.app'
   binary "#{appdir}/SeKey.app/Contents/MacOS/sekey"
+  pkg "SeKey-#{version}.pkg"
 
   postflight do
     launchd_plist = "#{ENV['HOME']}/Library/LaunchAgents/com.ntrippar.sekey.plist"
@@ -19,37 +19,6 @@ cask 'sekey' do
                    print_stdout: false,
                    print_stderr: false
 
-    IO.write launchd_plist, <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-          <key>EnvironmentVariables</key>
-          <dict>
-            <key>PATH</key>
-            <string>/usr/bin:/bin:/usr/sbin:/sbin</string>
-          </dict>
-          <key>Label</key>
-          <string>com.ntrippar.sekey</string>
-          <key>ProgramArguments</key>
-          <array>
-              <string>#{appdir}/SeKey.app/Contents/MacOS/sekey</string>
-              <string>--daemon</string>
-          </array>
-          <key>StandardErrorPath</key>
-          <string>#{HOMEBREW_PREFIX}/var/log/#{token}/#{token}.err.log</string>
-          <key>StandardOutPath</key>
-          <string>#{HOMEBREW_PREFIX}/var/log/#{token}/#{token}.out.log</string>
-          <key>KeepAlive</key>
-          <true/>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>ThrottleInterval</key>
-          <integer>30</integer>
-      </dict>
-      </plist>
-    EOS
-
     system_command '/bin/launchctl',
                    args: ['load', launchd_plist]
   end
@@ -58,7 +27,6 @@ cask 'sekey' do
 
   zap trash: [
                '~/.sekey',
-               "#{HOMEBREW_PREFIX}/var/log/#{token}",
              ]
 
   caveats <<~EOS
